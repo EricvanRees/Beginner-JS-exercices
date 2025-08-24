e463e81e6f9839397cf54646ad3770ee2c5ce569
Eric van Rees <ericvanrees@gmail.com>
2025-08-24 12:29:20 +0200

    add JS array scripts

---
+++ JS Array/30 Merge Arrays Without Duplicates.js
@@ -0,0 +1,21 @@
+/*
+Write a JavaScript function that merges two arrays and removes all duplicate elements.
+
+Test data :
+
+var array1 = [1, 2, 3];
+var array2 = [2, 30, 1];
+
+console.log(merge_array(array1, array2));
+[3, 2, 30, 1]
+*/
+
+function mergeArrs(arr1, arr2) {
+  const mergedArray = [...arr1, ...arr2];
+  const newSet = new Set(mergedArray);
+  return Array.from(newSet);
+}
+
+var array1 = [1, 2, 3];
+var array2 = [2, 30, 1];
+console.log(mergeArrs(array1, array2));
---
+++ JS Array/31 Remove Specific Element.js
@@ -0,0 +1,20 @@
+/* 
+Write a JavaScript function to remove a specific element from an array.
+
+Test data :
+
+console.log(remove_array_element([2, 5, 9, 6], 5));
+[2, 9, 6]
+*/
+
+
+function filterArr(anArr, anInt) {
+  const result = anArr.filter((el) =>
+    el !== anInt)
+  return result;
+}
+
+console.log(filterArr([2, 5, 9, 6], 5));
+
+
+
