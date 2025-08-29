e837d74ed11bee1bcd4051432aaecf49f737e84e
Eric van Rees <ericvanrees@gmail.com>
2025-08-29 09:04:37 +0200

    add JS DOM manipulation script

--- JS DOM/11 Random Image Display/script.js
+++ JS DOM/11 Random Image Display/script.js
@@ -1,11 +1,11 @@
 function myFunction() {
-  const div = document.createElement('div');
 
   let imgArr = [`<img src="http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg", width: "240", height: "160" alt="">`, 
-    `<img src="http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg", width: "320", height: "195">`, 
-    `<img src="http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg", width: "500", height: "343">`];
+    `<img src="http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg", width: "240", height: "160">`, 
+    `<img src="http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg", width: "240", height: "160">`];
 
-  div.innerHTML = imgArr[Math.floor(Math.random() * (imgArr.length))];
+  let div = document.querySelector('div');
+    div.innerHTML = imgArr[Math.floor(Math.random() * (imgArr.length))];
 
   document.body.appendChild(div);
 }
