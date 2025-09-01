2e54b303bdc31428cd13ed5dadfa8fd547fc8ab8
Eric van Rees <ericvanrees@gmail.com>
2025-09-01 08:04:25 +0200

    add JS Math scripts

---
+++ JS Math/06 Find Maximum in Array.js
@@ -0,0 +1,15 @@
+/* 
+Write a JavaScript function to find the highest value in an array.
+Test Data :
+console.log(max([12,34,56,1]));
+console.log(max([-12,-34,0,-56,-1]));
+56
+0
+*/
+
+function max(anArr) {
+  return Math.max(...anArr);
+}
+
+console.log(max([12,34,56,1]));
+console.log(max([-12,-34,0,-56,-1]));
---
+++ JS Math/07 Find Minimum in Array.js
@@ -0,0 +1,15 @@
+/* 
+Write a JavaScript function to find the lowest value in an array.
+Test Data :
+console.log(min([12,34,56,1]));
+console.log(min([-12,-34,0,-56,-1]));
+1
+-56
+*/
+
+function min(anArr) {
+  return Math.min(...anArr);
+}
+
+console.log(min([12,34,56,1]));
+console.log(min([-12,-34,0,-56,-1]));
