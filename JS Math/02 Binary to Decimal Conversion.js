593fa31e1a89ad39160b3b9e0f35a028ef6a9b8a
Eric van Rees <ericvanrees@gmail.com>
2025-09-01 07:39:44 +0200

    add JS Math examples

---
+++ JS Math/02 Binary to Decimal Conversion.js
@@ -0,0 +1,15 @@
+/* 
+Write a JavaScript function to convert a binary number to a decimal number.
+Test Data :
+console.log(bin_to_dec('110011'));
+console.log(bin_to_dec('100'));
+51
+4
+*/
+
+function bin_to_dec(aStr) {
+ return parseInt(aStr, 2);
+}
+
+console.log(bin_to_dec('110011'));
+console.log(bin_to_dec('100'));
