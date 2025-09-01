4758731112c2e74175282a26e5c0094b96f70adc
Eric van Rees <ericvanrees@gmail.com>
2025-09-01 08:22:04 +0200

    add JS Math scripts

---
+++ JS Math/15 Check If Value is Integer.js
@@ -0,0 +1,26 @@
+/* 
+Write a JavaScript function to check whether a value is an integer or not.
+Note : Integer - A number which is not a fraction; a whole number.
+Test Data :
+console.log(is_Int(23));
+console.log(is_Int(4e2));
+console.log(is_Int(NaN));
+console.log(is_Int(23.75));
+console.log(is_Int(-23));
+Output :
+true
+true
+false
+false
+true
+*/
+
+function is_Int(any) {
+  return Number.isInteger(any);
+}
+
+console.log(is_Int(23));
+console.log(is_Int(4e2));
+console.log(is_Int(NaN));
+console.log(is_Int(23.75));
+console.log(is_Int(-23));
