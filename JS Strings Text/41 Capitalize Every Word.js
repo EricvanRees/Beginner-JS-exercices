0a4c85fbe3f9d51c432d5f166d09fea14484cbaf
Eric van Rees <ericvanrees@gmail.com>
2025-09-22 07:07:47 +0200

    add JS text string examples

---
+++ JS Strings Text/41 Capitalize Every Word.js
@@ -0,0 +1,14 @@
+/* 
+Write a JavaScript function to capitalize each word in the string.
+
+Test Data :
+
+console.log(capitalizeWords('js string exercises'));
+"JS STRING EXERCISES"
+*/
+
+function capitalizeWords(astr) {
+  return astr.toUpperCase()
+}
+
+console.log(capitalizeWords('js string exercises'));
---
+++ JS Strings Text/42 Uncapitalize All Words.js
@@ -0,0 +1,14 @@
+/* 
+Write a JavaScript function to uncapitalize each word in the string.
+
+Test Data :
+
+console.log(unCapitalizeWords('JS STRING EXERCISES'));
+"js string exercises"
+*/
+
+function unCapitalizeWords(astr) {
+  return astr.toLowerCase()
+}
+
+console.log(unCapitalizeWords('JS STRING EXERCISES'));
