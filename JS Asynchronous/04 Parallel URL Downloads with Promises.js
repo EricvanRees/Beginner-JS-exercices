96625544d26062ef47153a3167998212e023ba08
Eric van Rees <ericvanrees@gmail.com>
2025-09-29 07:09:36 +0200

    add catch statement to Promise.all() example

--- JS Asynchronous/04 Parallel URL Downloads with Promises.js
+++ JS Asynchronous/04 Parallel URL Downloads with Promises.js
@@ -3,17 +3,29 @@ Write a JavaScript function that takes an array of URLs and downloads the conten
 */
 
 const urls = [
-  'https://jsonplaceholder.typicode.com/comments/1',
+  'https://jsonplaceholder.typicode.com/comment/1',
   'https://jsonplaceholder.typicode.com/comments/2',
   'https://jsonplaceholder.typicode.com/comments/3'
 ]
 
 function myFetch() {
-  Promise.all(urls.map(url => fetch(url).then((response) => response.json())))
-  .then((data) => {
-    // use forEach() to only log the body of the entire json response object
-    data.forEach(item => console.log(item.body))
+  Promise.all(
+    urls.map(url => 
+      fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status} at ${url}`);
+        }
+        return response.json();
+      })
+    )
+  )
+  .then(data => {
+    data.forEach(item => console.log(item.body));
   })
+  .catch(error => {
+    console.log("Fetch failed:", error.message);
+  });
 };
 
 myFetch()
