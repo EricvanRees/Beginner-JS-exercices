9175df02a8a4a0cafc42bc2bded1415b23eaf161
Eric van Rees <ericvanrees@gmail.com>
2025-09-30 11:57:40 +0200

    add JS Async Await Try Catch example script

--- JS Promises Async Await/08 Error Handling in Async Await.js
+++ JS Promises Async Await/08 Error Handling in Async Await.js
@@ -2,3 +2,21 @@
 Write a JavaScript program to demonstrate how to handle errors in async/await functions using try...catch.
 */
 
+const getUsers = async () => {
+  try {
+    const response = await fetch('https://httpbin.org/status/500');
+
+    if (!response.ok) {
+      throw new Error('Request Failed');
+    }
+
+    const data = await response.text();
+
+    console.log(data);
+  } 
+  catch (error) {
+    console.log(error);
+  }
+};
+
+getUsers();
