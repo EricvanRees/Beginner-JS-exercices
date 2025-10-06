516fed32e494c0c6577a448e305aef6ff5a0d8d2
Eric van Rees <ericvanrees@gmail.com>
2025-10-06 11:57:38 +0200

    add JS module script example

---
+++ Module Import Export/10 Aggregating Exports/basic arithmetics.js
@@ -0,0 +1,2 @@
+export {add, subtract} from './module 1.js';
+export {multiply, divide} from './module 2.js'; 
---
+++ Module Import Export/10 Aggregating Exports/script.js
@@ -0,0 +1,10 @@
+/* 
+Write a JavaScript program to aggregate exports from two different modules into one module and use them.
+*/
+
+import {add, subtract, divide, multiply} from './basic arithmetics.js'
+
+console.log(add(9, 5));
+console.log(subtract(9, 5));
+console.log(multiply(9, 5));
+console.log(divide(9, 9));
